0f559eeeff0585d561956e6be67c764ee0940cb7
smuminovic <44192671+smuminovic@users.noreply.github.com>
2018-12-31 22:52:51 +0100

    klasa Grad

---
+++ src/ba/unsa/etf/rpr/Grad.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr;
+
+public class Grad {
+    private long id;
+    private String naziv;
+    private long gradId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public long getGradId() {
+        return gradId;
+    }
+
+    public void setGradId(long gradId) {
+        this.gradId = gradId;
+    }
+}
