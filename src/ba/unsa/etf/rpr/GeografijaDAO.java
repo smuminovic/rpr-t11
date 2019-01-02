45907b81efaa433b147dac6adfece102152c8c99
smuminovic <44192671+smuminovic@users.noreply.github.com>
2019-01-02 18:43:04 +0100

    obrisani neki zakomentarisani dijelovi koda

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -113,10 +113,8 @@ public class GeografijaDAO implements DAO {
 
     @Override
     public Grad glavniGrad(String drzava) {
-        //ovo ispravit da daje glavni grad????
         String sql = "select id, naziv, glavni_grad from drzava where naziv = ?";
         try {
-            long idGlavnogGrada;
             PreparedStatement stmt = connection.prepareStatement(sql);
             stmt.setString(1, drzava);
             ResultSet set = stmt.executeQuery();
@@ -126,8 +124,7 @@ public class GeografijaDAO implements DAO {
                 d.setNaziv(set.getString(2));
                 d.setGradId(set.getLong(3));
             }
-            ArrayList<Grad> gradovi = new ArrayList<>();
-            gradovi = gradovi();
+            ArrayList<Grad> gradovi = gradovi();
             for (Grad grd:gradovi) {
                 if (d.getGradId() == grd.getId()) return grd;
             }
@@ -215,25 +212,12 @@ public class GeografijaDAO implements DAO {
 
     @Override
     public void dodajGrad(Grad grad) {
-        ArrayList<Drzava> drzave = drzave();
-        long drId = 0;
-        if (drzave.size() == 0) {
-            drId = drzave.size()+1;
-        }
-        else {
-            drId = drzave.size();
-        }
-        for (Drzava d:drzave) {
-            if (d.getNaziv().equals(grad.getDrzava().getNaziv())) {
-                drId = d.getId();
-            }
-        }
         String sql = "insert into grad(naziv, broj_stanovnika, drzava) values (?, ?, ?)";
         try {
             PreparedStatement stmt = connection.prepareStatement(sql);
             stmt.setString(1, grad.getNaziv());
             stmt.setInt(2, grad.getBrojStanovnika());
-            stmt.setLong(3, drId); //grad.getDrzavaId()
+            stmt.setLong(3, grad.getDrzavaId());
             stmt.execute();
             grad.setId(lastInsertedId());
         } catch (SQLException e) {
@@ -256,24 +240,11 @@ public class GeografijaDAO implements DAO {
 
     @Override
     public void dodajDrzavu(Drzava drzava) {
-        ArrayList<Grad> gradovi = gradovi();
-        long grId = 0;
-        if (gradovi.size() == 0) {
-            grId = gradovi.size()+1;
-        }
-        else {
-            grId = gradovi.size();
-        }
-        for (Grad g:gradovi) {
-            if (g.getNaziv().equals(drzava.getGlavniGrad().getNaziv())) {
-                grId = g.getId();
-            }
-        }
         String sql = "insert into drzava (naziv, glavni_grad) values (?, ?)";
         try {
             PreparedStatement stmt = connection.prepareStatement(sql);
             stmt.setString(1, drzava.getNaziv());
-            stmt.setLong(2, grId); //drzava.getGradId()
+            stmt.setLong(2, drzava.getGradId()); 
             stmt.execute();
             drzava.setId(lastInsertedId());
         } catch (SQLException e) {
