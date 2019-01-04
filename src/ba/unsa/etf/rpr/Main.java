c6919ef7eeabf4fb669ff7964c0dcef11e26f175
smuminovic <44192671+smuminovic@users.noreply.github.com>
2019-01-04 09:34:18 +0100

    dodan jednostavan gui

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,10 +1,17 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends Application {
     public static String ispisiGradove() {
         String rez = "";
         GeografijaDAO.removeInstance();
@@ -17,6 +24,20 @@ public class Main {
         }
         return rez;
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("geografija.fxml"));
+        loader.setController(new GeografijaController());
+        Parent root = loader.load();
+        primaryStage.setTitle("Geografija - RPR tutorijal 9");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
     public static void main(String[] args) {
+        launch(args);
     }
 }
